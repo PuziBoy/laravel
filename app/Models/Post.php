511de36208f75ed7c6d113dbe7df334dcae56fd6
PuziBoy <freaksaur2003>
2021-10-25 15:36:45 +0300

    add users and login

---
+++ app/Models/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['body', 'title', 'image'];
+//    public function getTitleAttribute($value){
+//        return ucfirst($value) . '.';
+//    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function setImageAttribute(UploadedFile $image){
+       $path = $image->store('public');
+       $this->image_path = Storage::url($path);
+    }
+
+    public function getSnippetAttribute(){
+        return explode("\n\n", $this->body)[0];
+    }
+
+    public function getDisplayBodyAttribute() {
+        return nl2br($this->body);
+    }
+
+    // @todo remove image when model is deleted
+}
